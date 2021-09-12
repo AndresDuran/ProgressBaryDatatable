5b41e4e5ea67942f5ab075568bb67eb0e47009e3
AndresDuran <polux_@outlook.com>
2021-09-12 08:25:34 -0500

    Se agrega funcional la barra de progreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressbarComponent } from './pages/progressbar/progressbar.component';
 import { DatatableComponent } from './pages/datatable/datatable.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    MatProgressBarModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
