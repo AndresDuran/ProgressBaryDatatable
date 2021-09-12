bfbab26b31ab01f3e5991b02c549a99a8a96ec38
AndresDuran <polux_@outlook.com>
2021-09-12 07:21:09 -0500

    Estructura inicial de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressbarComponent } from './pages/progressbar/progressbar.component';
+import { DatatableComponent } from './pages/datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressbarComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule,
